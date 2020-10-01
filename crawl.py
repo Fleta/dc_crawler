6dc6d0f2f121633a4de61fc5bf563076e1b4e82c
Fleta <yasoueisin@gmail.com>
2020-10-01 14:16:30 +0900

    add: function which finds range of yesterday's post

--- crawl.py
+++ crawl.py
@@ -3,6 +3,7 @@ from util import Helper
 import requests
 
 class Crawl:
+    # TODO: 클래스 용도 세분화(gall_list, gall_inside 등의 이름이 너무 불분명해 보임)
     def __init__(self):
         self.gall_base_url = "https://gall.dcinside.com/board/lists/"
         self.mgall_base_url = "https://gall.dcinside.com/mgallery/board/lists/"
@@ -29,16 +30,35 @@ class Gall_lists(Crawl):
                 print(atag.string, atag['href'].split('=')[1])
             except(Exception):
                 pass
-        
+
+    def collect_page_post(self, gall_code, page_num):
+        """ 
+        gall_code: 서치할 갤러리의 코드
+        page_num: 서치할 페이지 번호
+
+        return: (bs4.BeautifulSoup) 
+        """
+        payload = Helper().make_params(id=gall_code, page=page_num)
+        resp = requests.get(self.gall_base_url, headers=self.headers, params=payload)
+        print(type(BeautifulSoup(resp.content, 'html.parser')))
+        return BeautifulSoup(resp.content, 'html.parser')
+
 
 class Gall_inside(Crawl):
     def __init__(self):
         super().__init__()
 
-    def find_yesterday_post(self, gall_code):
-        payload = Helper().make_params(id=gall_code)
-        resp = requests.get(self.gall_base_url, headers=self.headers, params=payload)
-        soup = BeautifulSoup(resp.content, 'html.parser')
+    def find_yesterday_post(self, gall_code, page_num, first_post_num, last_post_num):
+        """
+        gall_code: 서치할 갤러리의 코드
+        page_num: 서치할 페이지 번호(그 페이지 안에서 서치 못 하면 다음 페이지로 넘겨서 재귀)
+        first_post_num: 어제의 첫 포스트 번호
+        last_post_num: 어제의 마지막 포스트 번호
+        
+        return: (first_post_num, last_post_num)
+        """
+
+        soup = collect_page_post(gall_code, page_num)
         post_list = soup.select('div.gall_listwrap > table.gall_list > tbody > tr.us-post') 
         # ub-content는 설문, 뉴스 포함
         # data-type기준 icon_notice=공지, icon_txt=짤 없는 글, icon_pic=짤 있는 글
@@ -48,9 +68,19 @@ class Gall_inside(Crawl):
             else:
                 # print(post.select('td.gall_num')[0].string, post.select('td.gall_date')[0]['title'])
                 post_time_str = post.select('td.gall_date')[0]['title']
-                if Helper().is_post_today(post_time_Str):
-                    # WIP
-                    pass
-                elif Helper().is_post_yesterday(post_time_str):
-                    # WIP
-                    pass
+                if Helper().is_post_yesterday(post_time_str):
+                    if last_post_num == -1:
+                        last_post_num = post.select('td.gall_num')[0].string 
+                else:
+                    if Helper().is_post_today(post_time_Str):
+                        pass
+                    elif first_post_num == -1:
+                        first_post_num = post.select('td.gall_num')[0].string 
+                    else:
+                        break
+        if not (first_post_num == -1 or last_post_num == -1):
+            # TODO: have to search another page - WIP
+            find_yesterday_post(gall_code, page_num, first_post_num, last_post_num)
+        else:
+            return (first_post_num, last_post_num)
+        
