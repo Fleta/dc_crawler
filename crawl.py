86e62d0bfec7c30c3cc34dd8f0dd425cabad0044
Fleta <yasoueisin@gmail.com>
2020-10-08 18:11:29 +0900

    fix: distinguish single and double quote

--- crawl.py
+++ crawl.py
@@ -7,7 +7,7 @@ class Crawl:
         self.gall_base_url = "https://gall.dcinside.com/board/lists/"
         self.mgall_base_url = "https://gall.dcinside.com/mgallery/board/lists/"
         self.user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36"
-        self.headers = {'User-Agent': self.user_agent, 'Accpet': 'text/*', 'Accept-Charset': 'utf-8'}
+        self.headers = {"User-Agent": self.user_agent, "Accpet": "text/*", "Accept-Charset": "utf-8"}
         
 
 class Gall_crawler(Crawl):
@@ -15,18 +15,16 @@ class Gall_crawler(Crawl):
         super().__init__()
 
     def collect(self, isMinor):
-        #cate_bind > cate_box > section_cate > cate_tit(=제목), list_num(=갤 개수)
+        # cate_bind > cate_box > section_cate > cate_tit(=제목), list_num(=갤 개수)
         url = "https://gall.dcinside.com/"
-        # if isMinor:
-        #     url = Helper.concat(url, 'm')
-        # 마갤 구조는 비슷한데 갯수가 너무 많아서 (약 1만6천) 전체 탐색은 의미 없음. 날짜별 실북갤만 수집하는게 좋을 듯
+        # 마갤 구조는 비슷한데 갯수가 너무 많아서 (약 1만6천) 전체 탐색은 의미 없음. 
 
         resp = requests.get(url, headers=self.headers)
-        soup = BeautifulSoup(resp.content, 'html.parser')
-        title_atag_list = soup.select('div.section_cate > div.cate > ul > li > a')
+        soup = BeautifulSoup(resp.content, "html.parser")
+        title_atag_list = soup.select("div.section_cate > div.cate > ul > li > a")
         for atag in title_atag_list:
             try:
-                print(atag.string, atag['href'].split('=')[1])
+                print(atag.string, atag["href"].split('=')[1])
             except(Exception):
                 pass
 
@@ -53,10 +51,10 @@ class Gall_crawler(Crawl):
         """
 
         soup = self.collect_page_post(gall_code, page_num)
-        post_list = soup.select('div.gall_listwrap > table.gall_list > tbody > tr.us-post') 
+        post_list = soup.select("div.gall_listwrap > table.gall_list > tbody > tr.us-post") 
         # ub-content는 설문, 뉴스 포함
         # data-type기준 icon_notice=공지, icon_txt=짤 없는 글, icon_pic=짤 있는 글
-        last_post_strtime = post_list[len(post_list) - 1].select('td.gall_date')[0]['title']
+        last_post_strtime = post_list[len(post_list) - 1].select("td.gall_date")[0]["title"]
         is_last_post_recursion = False
         is_first_post_recursion = False
         
@@ -89,10 +87,10 @@ class Gall_crawler(Crawl):
                         if is_first_post_recursion:
                             pass
                         elif last_post_num == -1:
-                            last_post_num = int(post.select('td.gall_num')[0].string)
+                            last_post_num = int(post.select("td.gall_num")[0].string)
                     elif is_first_post_recursion:
                         if first_post_num == -1:
-                            first_post_num = int(post.select('td.gall_num')[0].string)
+                            first_post_num = int(post.select("td.gall_num")[0].string)
                         else:
                             break
             if is_first_post_recursion and first_post_num == -1:
